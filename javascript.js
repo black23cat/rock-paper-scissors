fb7d76b06c28be56421509e012415800a0f70d23
Prayogi <fmother731@gmail.com>
2024-06-07 15:08:24 +0700

    Change playRound function to if else conditional due to bug when user input same value 3 times it return undefined

--- javascript.js
+++ javascript.js
@@ -1,31 +1,31 @@
 function getComputerChoice() {
-    let a = Math.floor(Math.random() * 6),
-        b;
-    if (a >= 0 && a <= 2){
-      b = 'rock'
-    }else if (a >= 3 && a <= 4){
-      b = 'paper'
-    }else{
-      b = 'scissors'
-    }
-    return b;
-  }
+  let a = Math.floor(Math.random() * 3);
+  return (a === 1)? 'rock'
+	:(a === 2)? 'paper'
+	:'scissors';
+}
 
 function getHumanChoice(){
-    let choice =  prompt('Choose your weapon Rock, Paper or Scissors.');
-    return choice.toLowerCase() || alert ('Canceled');
+  let choice =  prompt('Choose your weapon Rock, Paper or Scissors.');
+  return choice.toLowerCase();
 }
 
 function playRound(humanChoice, computerChoice){
-    let message =  (humanChoice === computerChoice) ?  'Draw!'
-    : (humanChoice === 'rock') && (computerChoice === 'paper') ? 'You Lose, Paper beat Rock'
-    : (humanChoice === 'rock') && (computerChoice === 'scissors') ? 'You Win, Rock beat Scissors' 
-    : (humanChoice === 'paper') && (computerChoice === 'scissors') ? 'You Lose, Scissors beat Paper' 
-    : (humanChoice === 'paper') && (computerChoice === 'rock') ? 'You Win, Paper beat Rock' 
-    : (humanChoice === 'scissors') && (computerChoice === 'paper') ? 'You Win, Scissors beat Paper' 
-    : (humanChoice === 'scissors') && (computerChoice === 'Rock') ? 'You Lose, Rock beat Scissors' 
-    : 'Please choose rock, paper or scissors';
-    return message;
+	if (humanChoice === computerChoice){
+		return  `Draw! ${humanChoice.toUpperCase()} vs ${computerChoice.toUpperCase()}.`;
+	} else if (humanChoice === 'rock' && computerChoice === 'paper'){
+		return 'You Lose, Rock vs Paper';
+	} else if (humanChoice === 'rock' && computerChoice === 'scissors'){
+		return 'You Win, Rock vs Scissors';
+	} else if (humanChoice === 'paper' && computerChoice === 'scissors'){
+		return 'You Lose, Paper vs Scissors';
+	} else if (humanChoice === 'paper' && computerChoice === 'rock'){
+		return 'You Win, Paper vs Rock';
+	} else if (humanChoice === 'scissors' && computerChoice === 'paper'){
+		return 'You Win, Scissors vs Paper';
+	} else if (humanChoice === 'scissors' && computerChoice === 'rock'){
+		return 'You Lose, Scissors vs Rock';
+	}
 }
 
 let humanScore = 0;
@@ -33,16 +33,16 @@ let computerScore = 0;
 
 
 function playGame(){
-    playerSelection = getHumanChoice();
-    humanSelection = playerSelection.toLowerCase();
-    computerSelection = getComputerChoice();
-    result = playRound(humanSelection, computerSelection);
+  playerSelection = getHumanChoice();
+  humanSelection = playerSelection.toLowerCase();
+  computerSelection = getComputerChoice();
+  result = playRound(humanSelection, computerSelection);
 
-    if (result.startsWith('You Win')){
-        humanScore++;
-      } else if (result.startsWith('You Lose')){
-        computerScore++;
-      }
+  if (result.startsWith('You Win')){
+     humanScore++;
+  } else if (result.startsWith('You Lose')){
+    computerScore++;
+  }
 }
 
 playGame();
@@ -66,13 +66,13 @@ console.log(playRound(humanSelection, computerSelection));
 console.log(`Your score ${humanScore} vs Computer score ${computerScore}`);
 
 function finalResult(){
-    if (humanScore > computerScore){
-        return 'You win Congratulations!';
-    } else if (humanScore < computerScore){
-        return 'You Lose, Try again later.';
-    } else {
-        return 'Tie';
-    }
+  if (humanScore > computerScore){
+    return 'You win Congratulations!';
+  } else if (humanScore < computerScore){
+    return 'You Lose, Try again later.';
+  } else {
+    return 'Tie';
+  }
 }
 
 console.log (finalResult())
