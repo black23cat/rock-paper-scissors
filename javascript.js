dcb04b9a89c318ec27c7cda8adda4cd5829289e0
Prayogi <prayogi.pangestu1299@gmail.com>
2024-06-13 15:39:53 +0700

    Add button to ui

--- javascript.js
+++ javascript.js
@@ -1,13 +1,54 @@
+const container = document.querySelector('#container');
+const rndResult = document.querySelector('.round-result');
+const score = document.querySelector('.score');
+const gameWinner = document.querySelector('.winner');
+const reset = document.querySelector('.reset')
+
+let humanScore = 0;
+let computerScore = 0;
+
+reset.addEventListener('click', () =>{
+  rndResult.textContent = "";
+  score.textContent = "";
+  gameWinner.textContent = "";
+  humanScore = 0;
+  computerScore = 0;
+});
+
+container.addEventListener('click', (e) => {
+  playGame();
+
+  function playGame(){
+    playerSelection = e.target.value;
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection);
+  
+    if (result.startsWith('You Win')){
+      humanScore++;
+    } else if (result.startsWith('You Lose')){
+      computerScore++;
+    }
+    rndResult.textContent = playRound(playerSelection, computerSelection);
+    score.textContent = `Your score ${humanScore} vs Computer score ${computerScore}`;
+  }
+
+  if(humanScore == 5 || computerScore == 5){
+    if (humanScore > computerScore){
+      gameWinner.textContent = 'You win Congratulations!';
+    } else {
+      gameWinner.textContent = 'You Lose, Try again later.';
+    }
+    // humanScore = 0;
+    // computerScore = 0;
+  }
+
+});
+
 function getComputerChoice() {
   let a = Math.floor(Math.random() * 3);
   return (a === 1)? 'rock'
 	:(a === 2)? 'paper'
 	:'scissors';
-}
-
-function getHumanChoice(){
-  let choice =  prompt('Choose your weapon Rock, Paper or Scissors.');
-  return choice.toLowerCase();
 }
 
 function playRound(humanChoice, computerChoice){
@@ -27,52 +68,3 @@ function playRound(humanChoice, computerChoice){
 		return 'You Lose, Scissors vs Rock';
 	}
 }
-
-let humanScore = 0;
-let computerScore = 0;
-
-
-function playGame(){
-  playerSelection = getHumanChoice();
-  humanSelection = playerSelection.toLowerCase();
-  computerSelection = getComputerChoice();
-  result = playRound(humanSelection, computerSelection);
-
-  if (result.startsWith('You Win')){
-     humanScore++;
-  } else if (result.startsWith('You Lose')){
-    computerScore++;
-  }
-}
-
-playGame();
-console.log(playRound(humanSelection, computerSelection));
-console.log(`Your score ${humanScore} vs Computer score ${computerScore}`);
-
-playGame();
-console.log(playRound(humanSelection, computerSelection));
-console.log(`Your score ${humanScore} vs Computer score ${computerScore}`);
-
-playGame();
-console.log(playRound(humanSelection, computerSelection));
-console.log(`Your score ${humanScore} vs Computer score ${computerScore}`);
-
-playGame();
-console.log(playRound(humanSelection, computerSelection));
-console.log(`Your score ${humanScore} vs Computer score ${computerScore}`);
-
-playGame();
-console.log(playRound(humanSelection, computerSelection));
-console.log(`Your score ${humanScore} vs Computer score ${computerScore}`);
-
-function finalResult(){
-  if (humanScore > computerScore){
-    return 'You win Congratulations!';
-  } else if (humanScore < computerScore){
-    return 'You Lose, Try again later.';
-  } else {
-    return 'Tie';
-  }
-}
-
-console.log (finalResult())
